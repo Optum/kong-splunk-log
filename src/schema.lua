986d4f79281680fb059ce0f511e610e18fc265ff
jeremyjpj0916 <31913027+jeremyjpj0916@users.noreply.github.com>
2018-06-01 12:07:16 -0400

    Create schema.lua

---
+++ src/schema.lua
@@ -0,0 +1,10 @@
+return {
+  fields = {
+    splunk_endpoint = { required = true,  default = "https://hec-splunk.optum.com/services/collector", type="url"},
+    method = { default = "POST", enum = { "POST", "PUT", "PATCH" } },
+    content_type = { default = "application/json", enum = { "application/json" } },
+    timeout = { default = 10000, type = "number" },
+    keepalive = { default = 60000, type = "number" },
+    splunk_access_token = { default = "aaaaaaaa-bbbb-cccc-dddd-ffffffffffff", type="text"}
+  }
+}

a5d246d718c95d31173bd36ddb9aaa037e862f7d
Pedro Fadel <pfadel@stone.com.br>
2018-10-08 13:25:44 -0300

    :bug: fix json format sent to splunk

--- splunk-log/basic.lua
+++ splunk-log/basic.lua
@@ -16,34 +16,38 @@ function _M.serialize(ngx)
 
   return {
     host = splunkHost,
-    request = {
-      uri = ngx.var.request_uri,
-      url = ngx.var.scheme .. "://" .. ngx.var.host .. ":" .. ngx.var.server_port .. ngx.var.request_uri,
-      querystring = ngx.req.get_uri_args(), -- parameters, as a table
-      method = ngx.req.get_method(), -- http method
-      headers = ngx.req.get_headers(),
-      size = ngx.var.request_length
-    },
-    upstream_uri = ngx.var.upstream_uri,
-    response = {
-      status = ngx.status,
-      headers = ngx.resp.get_headers(),
-      size = ngx.var.bytes_sent
-    },
-    tries = (ngx.ctx.balancer_address or EMPTY).tries,
-    latencies = {
-      kong = (ngx.ctx.KONG_ACCESS_TIME or 0) + (ngx.ctx.KONG_RECEIVE_TIME or 0) + (ngx.ctx.KONG_REWRITE_TIME or 0) +
-        (ngx.ctx.KONG_BALANCER_TIME or 0),
-      proxy = ngx.ctx.KONG_WAITING_TIME or -1,
-      request = ngx.var.request_time * 1000
-    },
-    authenticated_entity = authenticated_entity,
-    route = ngx.ctx.route,
-    service = ngx.ctx.service,
-    api = ngx.ctx.api,
-    consumer = ngx.ctx.authenticated_consumer,
-    client_ip = ngx.var.remote_addr,
-    started_at = ngx.req.start_time() * 1000
+    source = ngx.var.hostname,
+    sourcetype = "AccessLog",
+    event = {
+      request = {
+        uri = ngx.var.request_uri,
+        url = ngx.var.scheme .. "://" .. ngx.var.host .. ":" .. ngx.var.server_port .. ngx.var.request_uri,
+        querystring = ngx.req.get_uri_args(), -- parameters, as a table
+        method = ngx.req.get_method(), -- http method
+        headers = ngx.req.get_headers(),
+        size = ngx.var.request_length
+      },
+      upstream_uri = ngx.var.upstream_uri,
+      response = {
+        status = ngx.status,
+        headers = ngx.resp.get_headers(),
+        size = ngx.var.bytes_sent
+      },
+      tries = (ngx.ctx.balancer_address or EMPTY).tries,
+      latencies = {
+        kong = (ngx.ctx.KONG_ACCESS_TIME or 0) + (ngx.ctx.KONG_RECEIVE_TIME or 0) + (ngx.ctx.KONG_REWRITE_TIME or 0) +
+          (ngx.ctx.KONG_BALANCER_TIME or 0),
+        proxy = ngx.ctx.KONG_WAITING_TIME or -1,
+        request = ngx.var.request_time * 1000
+      },
+      authenticated_entity = authenticated_entity,
+      route = ngx.ctx.route,
+      service = ngx.ctx.service,
+      api = ngx.ctx.api,
+      consumer = ngx.ctx.authenticated_consumer,
+      client_ip = ngx.var.remote_addr,
+      started_at = ngx.req.start_time() * 1000
+    }
   }
 end
 

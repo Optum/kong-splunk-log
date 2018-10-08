ff1121b1e7c3298f4143bfc0059855bd87d78d54
Pedro Fadel <pfadel@stone.com.br>
2018-10-08 12:38:19 -0300

    :lipstick: format files

--- src/handler.lua
+++ src/handler.lua
@@ -19,12 +19,18 @@ local function generate_post_payload(method, content_type, parsed_url, body, spl
   else
     url = parsed_url.path
   end
-  local headers = string_format(
+  local headers =
+    string_format(
     "%s %s HTTP/1.1\r\nHost: %s\r\nConnection: Keep-Alive\r\nContent-Type: %s\r\nContent-Length: %s\r\n",
-    method:upper(), url, parsed_url.host, content_type, #body)
-  
+    method:upper(),
+    url,
+    parsed_url.host,
+    content_type,
+    #body
+  )
+
   if splunk_access_token ~= nil then
-    local auth_header = string_format("Authorization: Splunk %s\r\n",splunk_access_token)
+    local auth_header = string_format("Authorization: Splunk %s\r\n", splunk_access_token)
     headers = headers .. auth_header
   end
 
@@ -36,9 +42,9 @@ local function parse_url(host_url)
   if not parsed_url.port then
     if parsed_url.scheme == HTTP then
       parsed_url.port = 80
-     elseif parsed_url.scheme == HTTPS then
+    elseif parsed_url.scheme == HTTPS then
       parsed_url.port = 443
-     end
+    end
   end
   if not parsed_url.path then
     parsed_url.path = "/"
@@ -75,7 +81,6 @@ local function log(premature, conf, body, name)
   if not ok then
     ngx.log(ngx.ERR, name .. "failed to send data to " .. host .. ":" .. tostring(port) .. ": ", err)
   end
-  
 end
 
 -- Only provide `name` when deriving from this class. Not when initializing an instance.
--- src/schema.lua
+++ src/schema.lua
@@ -1,10 +1,10 @@
 return {
   fields = {
-    splunk_endpoint = { required = true,  default = "https://splunk.company.com/services/collector", type="url"},
-    method = { default = "POST", enum = { "POST", "PUT", "PATCH" } },
-    content_type = { default = "application/json", enum = { "application/json" } },
-    timeout = { default = 10000, type = "number" },
-    keepalive = { default = 60000, type = "number" },
-    splunk_access_token = { default = "aaaaaaaa-bbbb-cccc-dddd-ffffffffffff", type="text"}
+    splunk_endpoint = {required = true, default = "https://splunk.company.com/services/collector", type = "url"},
+    method = {default = "POST", enum = {"POST", "PUT", "PATCH"}},
+    content_type = {default = "application/json", enum = {"application/json"}},
+    timeout = {default = 10000, type = "number"},
+    keepalive = {default = 60000, type = "number"},
+    splunk_access_token = {default = "aaaaaaaa-bbbb-cccc-dddd-ffffffffffff", type = "text"}
   }
 }
